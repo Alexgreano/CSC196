a5d02e22db76f27ebd11393bbd7f2da585b89543
agomezreano <78039927+Alexgreano@users.noreply.github.com>
2021-06-30 16:02:44 -0600

    update

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,14 +1,54 @@
-#include "helper.h"
-#include "dynamic.h"
+#include "core.h"
 #include <iostream>
+#include <vector>
 
-int main() {
-	std::cout << nc::sqr(5) << std::endl;
+struct Vector2
+{
+	float x, y;
 
-	nc::point p1{ 10, 10 };
-	nc::point p2{ 10, 10 };
-	nc::point p3 = p1 + p2;
-	std::cout << p3.x << " " << p3.y << std::endl;
+	Vector2() : x{ 0 }, y{ 0 } {}
+	Vector2(float x, float y) : x{ x }, y{ y } {}
+};
 
-	system("pause");
+Vector2 position;
+std::vector<Vector2> points = { {0,0}, {10,0}, {5,10}, {0,0} };
+
+bool Update(float dt)
+{
+	bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
+
+	int x, y;
+	Core::Input::GetMousePos(x, y);
+	position.x = static_cast<float>(x);
+	position.y = static_cast<float>(y);
+
+	return quit;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	/*graphics.SetColor(RGB(255, 255, 255));*/
+	for (int i = 0; i < 20; i++) {
+		graphics.SetColor(RGB(rand() % 256 , rand() % 256 , rand() % 256 ));
+		//graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+	}
+	
+	graphics.SetColor(RGB(255, 255, 255));
+	//graphics.DrawLine(0, 0, position.x, position.y);
+
+	for (size_t i = 0; i < points.size() - 1; i++) {
+		graphics.DrawLine(points[i].x, points[i].y, points[i+1].x, points[i + 1].y);
+
+	}
+}
+
+int main()
+{
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+
+	Core::GameLoop();
+	Core::Shutdown();
 }
